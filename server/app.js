961e6b3d3f87701ab1ed4d748fa468fae613532b
Qomar034 <nurqomarudin034@gmail.com>
2023-02-18 13:13:15 +0700

    Install Express and Cors, init app.js

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.listen(port, () => console.log(`App Listening at port ${port}`))
